deae3e24bf4225121498508087c2f5b1c7fc4e0f
Your Name <you@example.com>
2021-09-26 12:09:02 -0400

    commit

--- public/index.js
+++ public/index.js
@@ -1,14 +1,4 @@
 // Making the single page application
-
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
-
 function Spa(){
     return (
         <HashRouter>
